3508e9cf12529de14573c60f40fcfd93674cec36
malongge <ma_longge@sina.com>
2016-12-17 17:02:48 +0800

    隐藏售价

--- update.bat
+++ update.bat
@@ -2,7 +2,5 @@ echo "-------update server start-----------------------"
 d:
 cd d:\project\inventory\
 git pull
-pip uninstall django-searchable-select
-pip uninstall django-selectable
 python manage.py migrate
 echo "----------update done------------------------"
