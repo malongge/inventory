87cb910c54f0d421407e0e192d9070d84d07283e
malongge <ma_longge@sina.com>
2016-12-17 15:03:43 +0800

    自动更新文件

--- update.bat
+++ update.bat
@@ -2,5 +2,7 @@ echo "-------update server start-----------------------"
 d:
 cd d:\project\inventory\
 git pull
+pip uninstall django-searchable-select
+pip uninstall django-selectable
 python manage.py migrate
 echo "----------update done------------------------"
