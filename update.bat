9c23dc6b19290b531cff0209967b7a5f7e6b0624
malongge <ma_longge@sina.com>
2017-02-15 10:51:12 +0800

    调整更新程序

--- update.bat
+++ update.bat
@@ -1,7 +1,8 @@
 echo "-------update server start-----------------------"
 d:
 cd d:\project\inventory\
-pip install django-suit -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 git pull
+pip install django-suit -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install django-braces -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 python manage.py migrate
 echo "----------update done------------------------"
