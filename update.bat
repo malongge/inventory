b1c618d3875fadb1f53f67747fce9dce40fa1297
malongge <ma_longge@sina.com>
2017-02-15 10:12:55 +0800

    订单里总价格显示清零

--- update.bat
+++ update.bat
@@ -1,6 +1,7 @@
 echo "-------update server start-----------------------"
 d:
 cd d:\project\inventory\
+pip install django-suit
 git pull
 python manage.py migrate
 echo "----------update done------------------------"
